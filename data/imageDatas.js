23435796a26766c1d002e28f9a41db0f0c2c4c5b
qq8697_whb <1058806283@qq.com>
2017-01-16 17:55:47 +0800

    更新图片和修改图片数据

--- data/imageDatas.js
+++ data/imageDatas.js
@@ -1,82 +1,82 @@
 var imageDatas=[
 	{
 		"fileName":"1.jpg",
-		"title":"title1",
-		"desc":"description1"
+		"title":"我好想在哪里见过你",
+		"desc":"我好像在哪见过你<br>我在劝我该忘了你<br><br><br><br><br>翔哥，新婚快乐！"
 	},
 	{
 		"fileName":"2.jpg",
-		"title":"title2",
-		"desc":"description2"
+		"title":"青山不改，绿水长流",
+		"desc":"亲们，我走了 <br>青山不改，绿水长流<br>咱们后会有期<br>我会想念你们的<br>有时间来重庆玩<br>我已经不知道怎样..."
 	},
 	{
 		"fileName":"3.jpg",
-		"title":"title3",
-		"desc":"description3"
+		"title":"大长腿拍摄技巧请咨询小鹏，联系方式见背面。",
+		"desc":"凭什么告诉你<br>o(￣ヘ￣o#)"
 	},
-	{
+	/*{
 		"fileName":"4.jpg",
-		"title":"title4",
-		"desc":"description4"
+		"title":"我左看看",
+		"desc":"对面的女孩看过来"
 	},
 	{
 		"fileName":"5.jpg",
-		"title":"title5",
-		"desc":"description5"
-	},
+		"title":"我右看看",
+		"desc":"对面的女孩看过来"
+	},*/
 	{
 		"fileName":"6.jpg",
-		"title":"title6",
+		"title":"那些年你们一起追过的女孩",
 		"desc":"description6"
 	},
 	{
 		"fileName":"7.jpg",
-		"title":"title7",
+		"title":"那些年你们一起追过的女孩",
 		"desc":"description7"
 	},
 	{
 		"fileName":"8.jpg",
-		"title":"title8",
+		"title":"小伙子，你内衣穿外面了",
 		"desc":"description8"
 	},
 	{
 		"fileName":"9.jpg",
-		"title":"title9",
+		"title":"官人，你看奴家美不美",
 		"desc":"description9"
 	},
 	{
 		"fileName":"10.jpg",
-		"title":"title10",
-		"desc":"description10"
+		"title":"我们组的猫呢？",
+		"desc":"猫有九命，唯有一心！"
 	},
 	{
 		"fileName":"11.jpg",
-		"title":"title11",
+		"title":"四环小王子和他的四个宠妃",
 		"desc":"description11"
 	},
 	{
 		"fileName":"12.jpg",
-		"title":"title12",
+		"title":"手牵手一步两步三步四步看镜头",
 		"desc":"description12"
 	},
 	{
 		"fileName":"13.jpg",
-		"title":"title13",
+		"title":"双生姐妹花~",
 		"desc":"description13"
 	},
 	{
 		"fileName":"14.jpg",
-		"title":"title14",
+		"title":"班长和班头",
 		"desc":"description14"
 	},
 	{
 		"fileName":"15.jpg",
-		"title":"title15",
+		"title":"'骄'不离'建'，'锐'不可当",
 		"desc":"description15"
-	},
+	}/*,
 	{
 		"fileName":"16.jpg",
-		"title":"title16",
+		"title":"论猥琐，我不是针对谁，在座的都是lj...",
 		"desc":"description16"
-	}
+	}*/
 ]
